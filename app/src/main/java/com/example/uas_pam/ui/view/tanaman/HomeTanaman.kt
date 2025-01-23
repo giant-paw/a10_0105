feec5ac62bf32bcacbda0176f5a0f31b9e527f40
giant-paw <giantpawbusiness@gmail.com>
2025-01-23 09:41:51 +0700

    menambahkan kode di homeTanaman

--- app/src/main/java/com/example/uas_pam/ui/view/tanaman/HomeTanaman.kt
+++ app/src/main/java/com/example/uas_pam/ui/view/tanaman/HomeTanaman.kt
@@ -1,19 +1,30 @@
 package com.example.uas_pam.ui.view.tanaman
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,11 +32,101 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas_pam.R
 import com.example.uas_pam.model.Tanaman
+import com.example.uas_pam.ui.viewmodel.tanaman.HomeTanamanState
+import com.example.uas_pam.ui.viewmodel.tanaman.HomeTanamanViewModel
 
+
+@Composable
+fun HomeStatusTanaman(
+    homeTanamanState: HomeTanamanState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Tanaman) -> Unit
+) {
+    when (homeTanamanState) {
+        is HomeTanamanState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeTanamanState.Success -> {
+            if (homeTanamanState.tanaman.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Tanaman")
+                }
+            } else {
+                TanamanLayout(
+                    tanaman = homeTanamanState.tanaman,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = onDetailClick,
+                    onDeleteClick = onDeleteClick
+                )
+            }
+        }
+        is HomeTanamanState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.connection_loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error), contentDescription = null
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = retryAction,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.primary)
+            )
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun TanamanLayout(
+    tanaman: List<Tanaman>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Tanaman) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(tanaman) { tnmn ->
+            TanamanCard(
+                tanaman = tnmn,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(tnmn.idtanaman) },
+                onDeleteClick = onDeleteClick
+            )
+        }
+    }
+}
 
 @Composable
 fun TanamanCard(
