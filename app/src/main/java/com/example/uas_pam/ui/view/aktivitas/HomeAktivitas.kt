90392225a172d3e44f292731680e2502ca2a7b97
giant-paw <giantpawbusiness@gmail.com>
2025-01-24 16:17:42 +0700

    tampilan home view untuk aktivitas

---
+++ app/src/main/java/com/example/uas_pam/ui/view/aktivitas/HomeAktivitas.kt
@@ -0,0 +1,250 @@
+package com.example.uas_pam.ui.view.aktivitas
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uas_pam.R
+import com.example.uas_pam.model.Aktivitas
+import com.example.uas_pam.ui.customwidget.TopAppBarr
+import com.example.uas_pam.ui.navigasi.DestinasiNavigasi
+import com.example.uas_pam.ui.viewmodel.aktivitas.HomeAktivitasState
+import com.example.uas_pam.ui.viewmodel.aktivitas.HomeAktivitasViewModel
+import com.example.uas_pam.ui.viewmodel.penyediamodel.PenyediaViewModel
+
+object DestinasiHomeAktivitas: DestinasiNavigasi {
+    override val route = "home_aktivitas"
+    override val titleRes = "Daftar Aktivitas"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeAktivitasScreen(
+    navigateBack: () -> Unit,
+    navigateToItemEntry: () -> Unit,
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier,
+    viewModel: HomeAktivitasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(Unit) {
+        viewModel.getAkt()
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarr(
+                title = DestinasiHomeAktivitas.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getAkt()
+                }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = navigateToItemEntry,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Tambah Aktivitas"
+                    )
+                },
+                text = {
+                    Text(text = "Tambah Aktivitas")
+                },
+                containerColor = colorResource(R.color.primary),
+                contentColor = Color.White,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    ) { innerPadding ->
+        HomeStatusAktivitas(
+            homeAktivitasState = viewModel.aktivitasHomeState,
+            retryAction = { viewModel.getAkt() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = { aktivitas ->
+                viewModel.deleteAkt(aktivitas.idaktivitas)
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatusAktivitas(
+    homeAktivitasState: HomeAktivitasState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Aktivitas) -> Unit
+) {
+    when (homeAktivitasState) {
+        is HomeAktivitasState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeAktivitasState.Success -> {
+            if (homeAktivitasState.aktivitas.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Aktivitas")
+                }
+            } else {
+                AktivitasLayout(
+                    aktivitas = homeAktivitasState.aktivitas,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = onDetailClick,
+                    onDeleteClick = onDeleteClick
+                )
+            }
+        }
+        is HomeAktivitasState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.connection_loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error), contentDescription = null
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = retryAction,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.primary)
+            )
+        ) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun AktivitasLayout(
+    aktivitas: List<Aktivitas>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    onDeleteClick: (Aktivitas) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(aktivitas) { akt ->
+            AktivitasCard(
+                aktivitas = akt,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(akt.idaktivitas) },
+                onDeleteClick = onDeleteClick
+            )
+        }
+    }
+}
+
+@Composable
+fun AktivitasCard(
+    aktivitas: Aktivitas,
+    onDeleteClick: (Aktivitas) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = aktivitas.tanggalaktivitas,
+                    fontSize = 18.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Create, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = aktivitas.deskripsi_aktivitas,
+                    fontSize = 18.sp
+                )
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = { onDeleteClick(aktivitas) }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Hapus")
+                }
+            }
+        }
+    }
+}
