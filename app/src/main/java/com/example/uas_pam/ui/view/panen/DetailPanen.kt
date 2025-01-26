f886ad6919eca04ab8724b1b955e301193fae7a7
giant-paw <giantpawbusiness@gmail.com>
2025-01-26 09:29:36 +0700

    tampilan view untuk detail panen

---
+++ app/src/main/java/com/example/uas_pam/ui/view/panen/DetailPanen.kt
@@ -0,0 +1,155 @@
+package com.example.uas_pam.ui.view.panen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.uas_pam.R
+import com.example.uas_pam.ui.customwidget.TopAppBarr
+import com.example.uas_pam.ui.navigasi.DestinasiNavigasi
+import com.example.uas_pam.ui.viewmodel.panen.DetailPanenState
+import com.example.uas_pam.ui.viewmodel.panen.DetailPanenViewModel
+import com.example.uas_pam.ui.viewmodel.penyediamodel.PenyediaViewModel
+
+object DestinasiDetailPanen : DestinasiNavigasi {
+    override val route = "detail_panen"
+    override val titleRes = "Detail Catatan Panen"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPanenScreen(
+    navigateBack: () -> Unit,
+    id_panen: String,
+    modifier: Modifier = Modifier,
+    viewModel: DetailPanenViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    navController: NavHostController
+) {
+    val panenState by viewModel.detailPanenState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(Unit) {
+        viewModel.getPanen(id_panen)
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarr(
+                title = DestinasiDetailPanen.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate("update_panen/$id_panen")
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp),
+                containerColor = colorResource(R.color.primary),
+                contentColor = Color.White
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Update Panen")
+            }
+        }
+    ) { innerPadding ->
+        DetailBodyPanen(
+            detailPanenState = panenState,
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun DetailBodyPanen(
+    detailPanenState: DetailPanenState,
+    modifier: Modifier = Modifier
+) {
+    when (detailPanenState) {
+        is DetailPanenState.Loading -> {
+            CircularProgressIndicator(modifier = modifier.fillMaxSize())
+        }
+        is DetailPanenState.Error -> {
+            Text(
+                text = detailPanenState.message,
+                color = Color.Red,
+                modifier = modifier.fillMaxSize().wrapContentSize(Alignment.Center)
+            )
+        }
+        is DetailPanenState.Success -> {
+            val panen = detailPanenState.panen
+            Column(
+                verticalArrangement = Arrangement.spacedBy(18.dp),
+                modifier = modifier.padding(12.dp)
+            ) {
+                ComponentDetailPanen(judul = "ID Tanaman", isinya = panen.idpanen)
+                ComponentDetailPanen(judul = "ID Tanaman", isinya = panen.idtanaman)
+                ComponentDetailPanen(judul = "ID Pekerja", isinya = panen.tanggalpanen)
+                ComponentDetailPanen(judul = "ID Pekerja", isinya = panen.jumlahpanen)
+                ComponentDetailPanen(judul = "ID Pekerja", isinya = panen.keterangan)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailPanen(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 19.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 19.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
